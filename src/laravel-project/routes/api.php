27e9ea2d02edfa65ad91a6498069e018ddde0474
ssagara00 <barabeikaso@yahoo.co.jp>
2023-10-14 12:02:17 +0900

    add login and logout by sanctum

--- src/laravel-project/routes/api.php
+++ src/laravel-project/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StudyRecordController;
 /*
@@ -16,10 +18,16 @@ use App\Http\Controllers\StudyRecordController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    //認証を必要とする機能
 });
 
-Route::apiResource('/users', UserController::class);
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
 
+Route::apiResource('/users', UserController::class);
 Route::apiResource('/studyrecords', StudyRecordController::class);
